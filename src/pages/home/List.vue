9711223d8162f2d965f7e6b5f16a6cd17b331ab1
裴智慧 <13269724310@163.com>
2019-05-27 13:12:02 +0800

    add fastclick and must styles

---
+++ src/pages/home/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class='home'>list</div>
+  </div>
+</template>
+
+<script>
+
+export default{
+  name: 'Home'
+}
+
+</script>
+
+<style>
+.home{
+  font-size: 50px;
+}
+</style>
